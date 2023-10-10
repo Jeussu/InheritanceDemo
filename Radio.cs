3fed27cb6206ddf670e27ec24cc6f5ecf7f3a860
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-10-10 22:42:43 +0700

    Add project files.

---
+++ Radio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    //Child class
+    internal class Radio : ElectricalDevice
+    {
+
+        public Radio(bool isOn, string brand) : base(isOn, brand)
+        {
+            
+        }
+
+        public void ListenRadio()
+        {
+            //first check if the radio is on
+            if (IsOn)
+            {
+                // listen to radio
+                Console.WriteLine("Listening to the Radio!");
+            }
+            else
+            {
+                //print error message
+                Console.WriteLine("Radio is switched off, switch it on first");
+            }        
+        }
+    }
+}
