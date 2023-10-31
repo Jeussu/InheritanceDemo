02c12e110bd813ff60a3ef4620da0efa58613549
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-10-31 10:41:39 +0700

    31/10

---
+++ InheritanceChallenge2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace InheritanceChallenge2
+{
+    internal class Program
+    {
+        //Create a main class with the Main method, then a base class employee with the properties Name,
+        // FirstName, Salary and the methods Work() and Pause().
+        // Create a deriving class boss with the propertie CompanyCar and method Lead().
+        // Create another deriving class of employees - trainees with the properties workingHours
+        //and SchoolHours and a method Learn();
+        //Override the methods Works() of the trainee class so that it indicates
+        // the working hours of the trainee.
+        //Don't forget to create the constructors.
+        //Create an object of each of the three classes (with arbtrary values)
+        //and call the methods, Lead() of Boss and Work() of Trainee.
+        // Just print out the respective text, what the respective employees do.
+        static void Main(string[] args)
+        {
+            Employee michael = new Employee("Michael", "Miller", 40000);
+
+            michael.Work();
+            michael.Pause();
+
+            Boss chuckNorris = new Boss("Ferrari", "Norris", "Chuck", 99999999);
+
+            chuckNorris.Lead();
+
+            Trainee michelle = new Trainee(32, 8, "Gartner", "Michelle", 10000);
+            michelle.Learn();
+            michelle.Work();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InheritanceChallenge2/Trainee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceChallenge2
+{
+    internal class Trainee : Employee
+    {
+        public int WorkingHours { get; set; }
+        public int SchoolHours { get; set; }
+
+        public Trainee(int workingHours, int schoolHours, string name, string firstName, int salary) : base(name, firstName, salary)
+        {
+            this.WorkingHours = workingHours;
+            this.SchoolHours = schoolHours;
+        }
+
+        public void Learn()
+        {
+            Console.WriteLine("I'm learning for {0} hours!", SchoolHours);
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("I'm work for {0} hours", WorkingHours);
+        }
+    }
+}
