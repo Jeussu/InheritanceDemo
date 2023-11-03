5fdb3c0679ecab7a2c2f33c8df46836101bfb774
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-11-03 21:15:54 +0700

    11/03

---
+++ InheritanceDemo3/Furniture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo3
+{
+    //base class for furnitures
+    internal class Furniture
+    {
+        //color of the furniture
+        public string Color { get; set; }
+        //material of the furniture
+        public string Material { get; set; }
+        //default constructor
+        public Furniture()
+        {
+            Color = "While";
+            Material = "Wood";
+        }
+
+        //simple constructor
+
+        public Furniture(string color, string material)
+        {
+            Color= color;
+            Material = material;
+        }
+    }
+}
---
+++ InheritanceDemo3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace InheritanceDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating two objects of type chair
+            Chair officeChair = new Chair("Brown", "Plastic");
+            Chair gamingChair = new Chair("Red", "Wood");
+
+            //creating a new object of type car
+            Car damagedCar = new Car(80f, "Blue");
+
+            //add the two chair to the IDestroyable list of the car
+            // so that when we destroy the car the destroyable object
+            // that are near the car will get destroyed as well
+            damagedCar.DestroyablesNearby.Add(officeChair);
+            damagedCar.DestroyablesNearby.Add(gamingChair);
+
+            //destory the car
+            damagedCar.Destroy();
+        }
+    }
+}
