5c943437f0e7639389ff2abf8a4cf63c14c04e12
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-11-12 22:06:27 +0700

    11/12

---
+++ IEnumerableDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace IEnumerableDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a generic IEnumerable variable that takes any collection of type int
+            //we will use this variable to store the collections that will get return by the Getcollection() method
+            IEnumerable<int> unknownCollection;
+            unknownCollection = GetCollection(1);
+
+            Console.WriteLine("This was a List<int>");
+            //for each number in the collection we got back from Getcollection(1);
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+
+            //new line 
+            Console.WriteLine("");
+            //call GetCollection() with option = 2 which will return a Queue<int> but we will store it in the base type of generic
+            // but we will store it in the base type of generic collection
+            unknownCollection = GetCollection(2);
+
+            Console.WriteLine("This was a Queue<int>");
+            //for each number in the collection we got back from Getcollection(2);
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+
+            //new line 
+            Console.WriteLine("");
+            //call GetCollection() with option = 5 which will return a Queue<int> but we will store it in the base type of generic
+            // but we will store it in the base type of generic collection
+            unknownCollection = GetCollection(5);
+
+            Console.WriteLine("This was an array of int");
+            //for each number in the collection we got back from Getcollection(2);
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        static IEnumerable<int> GetCollection(int option)
+        {
+            //create a list of numbers and initialize it
+            List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Queue<int> numbersQueue = new Queue<int>();
+            //add values to the queue
+            numbersQueue.Enqueue(6);
+            numbersQueue.Enqueue(7);
+            numbersQueue.Enqueue(8);
+            numbersQueue.Enqueue(9);
+            numbersQueue.Enqueue(10);
+
+            //if the option is 1
+            if (option == 1)
+            {
+                //return the list of type List<int>
+                return numbersList;
+                //if the option is 2
+            }
+            else if (option == 2)
+            {
+                //return the queue of type<int>
+                return numbersQueue;
+                //otherwise
+            }
+            else
+            {
+                //return an array of numbers initialized with some numbers
+                return new int[] { 11, 12, 13, 14, 15 };
+            }
+
+
+        }
+    }
+}
---
+++ IEnumerableDemo2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IEnumerableDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a list of type List<int> initialized with some number
+            List<int> numberList = new List<int>() { 8, 6, 2 };
+            int[] numberArray = new int[] { 1, 7, 1, 3 };
+            //call collectionSum() and pass the list to it 
+            CollectionSum(numberList);
+
+            Console.WriteLine(" ");
+            //call CollectionSum() and pass the array to it
+            CollectionSum(numberArray);
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection)
+        {
+            //sum variable to store the sum of the numbers in anycollection
+            int sum = 0;
+            //for each number in collection passed to this method
+            foreach (var num in anyCollection)
+            {
+                //add the num value to sum
+                sum += num;
+            }
+
+            //print the sum
+            Console.WriteLine("Sum is {0}", sum);
+        }
+    }
+}
